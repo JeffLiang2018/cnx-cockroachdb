ea0895f4625782fd386a818b397a24a6d0cf9a03
Jeff Liang <liangj@Jeffs-MacBook-Pro-4.local>
2023-08-25 16:55:37 -0700

    spike: connect to cockroachDB

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 import "fmt"
 
-//import "gorm.io/gen"
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
 	fmt.Println("Hello World, CNX CockroachDB!!!")
+	host := "10.164.0.189"
+	port := 26257
+	user := "pvos_dia_db_rw"
+	password := "c0f47960-9c78-4039-9760-c0ff390b3259_pvos_dia_db_rw"
+	database := "pvos_dia_db"
+	dns := fmt.Sprintf("host=%s user=%s password=%s database=%s port=%d sslmode=enable", host, user, password, database, port)
+	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	if err != nil {
+		panic(fmt.Sprintf("error happened when creating CockroachDB connection, err: %v", err))
+	}
+	if db != nil {
+		fmt.Println("CockroachDB connection is created successfully")
+	}
+	fmt.Println("CNX CockroachDB is connected successfully!!!!!")
+	rawDB, err := db.DB()
+	if err != nil {
+		panic(fmt.Sprintf("error happened when getting rawDB before closing connection, err: %v", err))
+	}
+	rawDB.Close()
+	fmt.Println("CockroachDB connection is closed successfully")
 }
